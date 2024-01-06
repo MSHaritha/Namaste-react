4b026b59ceaffccdccf7f660cd393d299abf4376
MSHaritha <harithamohanavel08@gmail.com>
2024-01-06 18:59:58 +0530

    episode-1

---
+++ README.md
@@ -0,0 +1,2 @@
+#Namaste React🚀
+In this journey of react,I will be uploading all my react learnings from scratch.
---
+++ app.js
@@ -0,0 +1,28 @@
+/* 
+<div id="parent">
+  <div id="child1">
+    <h1></h1>
+    <h2></h2>
+  </div>
+
+  <div id="child2">
+    <h1></h1>
+    <h2></h2>
+  </div>
+</div>
+*/
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "h1 tag from child1"),
+    React.createElement("h1", {}, "h1 tag from child1"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "h1 tag from child2"),
+    React.createElement("h1", {}, "h1 tag from child2"),
+  ]),
+]);
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
